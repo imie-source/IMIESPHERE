f437d7a56345163ec193f2579dd6febc3b3d6861
ThibaudCarie <thibaud.carie@gmail.com>
2015-02-06 13:23:45 +0100

    Avancement envoie mail + ajout des infos sur l'IMIE

--- Contacts/PHP/contact.php
+++ Contacts/PHP/contact.php
@@ -1,139 +1,77 @@
 <?php 
 
-	define('ENVOIE_OK', 1);
-	
-	require ("bdd_inc.php");
+	/**
+	 * Fonctions qui permettent d'envoyer un mail à l'association
+	 * Avec confirmation de l'envoie du mail
+	 *
+	 * @author Thibaud Carié
+	 * @since 05/01/2015
+	 */
+
+	define('ENVOI_KO', 1);
+
+	define('ENVOI_OK', 2);
+			
+
+	require_once ("bdd_inc.php");
+
+	require_once (__DIR__ . "/../../lib/PHPMailer/class.phpmailer.php");
 
 	/**
- 	 *
-	 * Fonction qui envoie un mail à la personne postant son message (accusé de reception)
+	 * Fonction qui configure PHPMailer et envoie le mail
+	 *
+	 * @return Vrai si le mail a été envoyé, Faux si il y a eu une erreur
 	 */
-	function accuseRecep() {
+	function mailPHPMailer() {
+		
+		$mail = new PHPMailer();
 
-		if (check_mail($_POST["emailaddress"])) {
+		$mail->IsSMTP();
 
-			// Déclaration de l'adresse de destination.
-			$mail = $_POST["emailaddress"];
+		$mail->SMTPAuth = true;
 
-			// On filtre les serveurs qui rencontrent des bogues.
-			if (!preg_match("/^[a-z0-9._-]+@(hotmail|live|msn)\.[a-z]{2,4}$/", $mail)) {
+		$mail->SMTPSecure = 'ssl';
 
-				$passage_ligne = "\r\n";
-			}
+		$mail->Host = 'smtp.gmail.com';
 
-			else {
+		$mail->Port = 465;
 
-				$passage_ligne = "\n";
-			}
+		$mail->CharSet = "utf-8";
 
-			//Déclaration du message au format HTML.
-			$message_html = "Bonjour, \nVotre message à bien été envoyé à l'association IMIESPHERE\n. Elle vous répondra dans les plus bref délais\n Cordialement l'équipe de l'IMIESPHERE";
-			 
-			//Création de la boundary
-			$boundary = "-----=".md5(rand());
-			 
-			//Définition du sujet.
-			$sujet = "Accusé de reception";
+		$mail->Username = USERNAME;
 
-			$nom = $_POST["nom"];
+		$mail->Password = PASSWORD;
 
-			$mailExp = $_POST["emailaddress"];
-			 
-			//Création du header de l'e-mail.
-			$header = "From: \"$nom\"$mailExp".$passage_ligne;
-			$header.= "Reply-to: \"$nom\"$mailExp".$passage_ligne;
-			$header.= "MIME-Version: 1.0".$passage_ligne;
-			$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-			 
-			//Création du message.
-			$message = $passage_ligne."--".$boundary.$passage_ligne;
+		$mail->SetFrom(USERNAME, "IMIE Sph&egrave;re");
 
-			//Ajout du message au format HTML
-			$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-			$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-			$message.= $passage_ligne.$message_html.$passage_ligne;
+		$mail->AddAddress($_POST["emailaddress"], $_POST["nom"]);
 
-			$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-			$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-			 
-			//Envoi de l'e-mail.
-			mail($mail,$sujet,$message,$header);
+		$mail->Subject = $_POST["objet"];
 
+		$mail->MsgHTML($_POST["msg"]);
+
+		if (!$mail->Send()) {
+
+			return ENVOI_KO;
+			
+		}
+
+		else {
+
+			return ENVOI_OK;
 		}
 	}
 
 	/**
 	 * Fonction qui check si le mail est valide
 	 *
+	 * @param $email La mail à vérifier
 	 * @return Vrai le mail est correct, faux si le mail n'est pas correct
 	 */
 	function check_mail($email){
 
 		// Utilisation expression régulière pour vérifier le mail
  		return(preg_match("/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/", $email));
-	}
-
-	/**
-	 * Fonction qui envoie le message à l'association
-	 *
-	 */
-	function envoieMail() {
-		
-		// Si le mail est correct
-		if (check_mail($_POST["emailaddress"])) {
-
-			// Déclaration de l'adresse de destination.
-			$mail = 'thibaud.carie@gmail.com';
-
-			// On filtre les serveurs qui rencontrent des bogues.
-			if (!preg_match("/^[a-z0-9._-]+@(hotmail|live|msn)\.[a-z]{2,4}$/", $mail)) {
-
-				$passage_ligne = "\r\n";
-			}
-
-			else {
-
-				$passage_ligne = "\n";
-			}
-
-			//Déclaration du message au format HTML.
-			$message_html = $_POST["msg"];
-			 
-			//Création de la boundary
-			$boundary = "-----=".md5(rand());
-			 
-			//Définition du sujet.
-			$sujet = $_POST["objet"];
-
-			$nom = $_POST["nom"];
-
-			$tel = $_POST["tel"];
-
-			$mailExp = $_POST["emailaddress"];
-			 
-			//Création du header de l'e-mail.
-			$header = "From: \"$nom\"$mailExp".$passage_ligne;
-			$header.= "Reply-to: \"$nom\"$mailExp".$passage_ligne;
-			$header.= "MIME-Version: 1.0".$passage_ligne;
-			$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-			 
-			//Création du message.
-			$message = $passage_ligne."--".$boundary.$passage_ligne;
-
-			//Ajout du message au format HTML
-			$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-			$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-			$message.= $passage_ligne.$tel.$passage_ligne.$message_html.$passage_ligne;
-
-			$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-			$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-			 
-			//Envoi de l'e-mail.
-			mail($mail,$sujet,$message,$header);
-
-			// On envoie un accusé de réception
-			accuseRecep();
-		}
 	}
 
 	/**
@@ -152,6 +90,7 @@
 		// Connexion à la base de données
 		$dbLink = cnxBDD($dbConf);
 
+		// Je définis le modèle de ma requête
 		$sql = "INSERT INTO Contact (nom, mail, tel, objet, msg) VALUES (:nom, :mail, :tel, :objet, :msg);";
 
 		$req = $dbLink->prepare($sql);
@@ -184,7 +123,26 @@
 		$dbLink = NULL;
 	}
 	
-	envoieMessage($_POST["nom"], $_POST["tel"], $_POST["emailaddress"], $_POST["objet"], $_POST["msg"]);
+	if (isset($_POST["send"])) {
 
-	envoieMail();
+		$res = mailPHPMailer();
+
+		$send = envoieMessage($_POST["nom"], $_POST["tel"], $_POST["emailaddress"], $_POST["objet"], $_POST["msg"]);
+		
+		switch ($res) {
+
+			case ENVOI_OK:
+				include (__DIR__ . "/../HTML/envoie_ok.html");
+				break;
+
+			case ENVOI_KO:
+				include (__DIR__ . "/../HTML/envoie_ko.html");
+				break;
+		}
+	}
+
+	else {
+
+		require (__DIR__ . "/../HTML/contact.html");
+	}
 ?>
